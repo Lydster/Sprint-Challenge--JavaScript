aaeba2ed2390a122a666a9a431e260fa9bc0cda3
lydiathornton <lydia.d.thornton@gmail.com>
2019-01-25 08:31:24 -0800

    created objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,12 +5,45 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
+class dino {
+  constructor(atts) {
+    this.name = atts.name;
+    this.diet = atts.diet;
+    this.weight = atts.weight;
+    this.length = atts.lengtt;
+    this.period = atts.period;
+  }
+}
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
 
+const tyrannosaurus = new dino({
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceious'
+})
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stegosaurus = new dino({
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+})
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+
+const velociraptor = new dino({
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceious'
+})
 
 // Using your dinosaur objects, log answers to these questions:
 
