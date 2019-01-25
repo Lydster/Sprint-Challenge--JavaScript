7f854a5b2e1f730f01f67fe78f39f7d432ea1a17
lydiathornton <lydia.d.thornton@gmail.com>
2019-01-25 10:22:10 -0800

    updated arrays in objects-arrays.js to arrow sytax

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -134,9 +134,7 @@ The zoo wants to display both the scientific name and the animal name in front o
 */
 const animalNames = [];
 
-  zooAnimals.forEach(function(animal) {
-    animalNames.push('Name: ' + animal.animal_name + ', Scientific: ' + animal.scientific_name)
-  })
+  zooAnimals.forEach(animal => animalNames.push('Name: ' + animal.animal_name + ', Scientific: ' + animal.scientific_name)) 
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -145,9 +143,8 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = zooAnimals.map(function(animal) {
-   return animal.animal_name.toLowerCase()
-})
+const lowerCase = zooAnimals.map(animal => animal.animal_name.toLowerCase()) 
+
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -165,14 +162,15 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
+/*
 const populationTotal = zooAnimals.reduce(function (total, zooAnimals) {
   return total + zooAnimals.population
 },0)
 console.log(populationTotal);
-
-
+*/
+const populationTotal = zooAnimals.reduce((total, zooAnimals) => total + zooAnimals.population, 0);
+console.log(populationTotal)
 /* 
-
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
